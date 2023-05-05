468b13e3e9191575faf0b1ab92f381cd7440e047
Eng-AbdooRagab <abdooragab322@gmail.com>
2023-05-05 17:36:54 +0200

    Design Editing

--- app/src/main/java/com/mindsoft/ui/activity/SplashscreenActivity.java
+++ app/src/main/java/com/mindsoft/ui/activity/SplashscreenActivity.java
@@ -3,6 +3,9 @@ package com.mindsoft.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +13,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.mindsoft.R;
 import com.mindsoft.data.repoistory.SystemRepository;
 import com.mindsoft.databinding.ActivitySplashscreenBinding;
 
+import java.util.concurrent.TimeUnit;
+
 public class SplashscreenActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
-
+    private static final int SPLASH_SCREEN_TIMEOUT = 4000;
+    ImageView logo;
+    View s1;
+    View s2;
+    View s3;
     private ActivitySplashscreenBinding binding;
 
     @Override
@@ -26,7 +35,26 @@ public class SplashscreenActivity extends AppCompatActivity {
         binding = ActivitySplashscreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        ImageView logo = binding.logo;
+
+
+        /////Declaration----
+         logo = binding.logo;
+         s1 = binding.view1;
+        s2 = binding.viwe2;
+        s3 = binding.viwe3;
+
+        ////----///Animations-------
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.base_line_anim);
+        logo.startAnimation(animation);
+         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.appere_left);
+        s1.startAnimation(animation);
+         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.appere_right);
+        s2.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.appere_right);
+        s3.startAnimation(animation);
+        ///Animations-------
+
+
 
         FirebaseApp.initializeApp(this);
 
